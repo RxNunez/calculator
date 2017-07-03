3c9d388f1e5b65a146f34a8bc278df9201e57f97
Maria Thomas and Ryan Nunez <maria@>
2017-07-03 10:11:26 -0700

    Add addition, subtraction, multiplication and division functions of javascripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Addition
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -6,3 +7,33 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
+
+//Subtraction
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(sub(number1, number2));
+
+//Multiplication
+var mult = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(mult(number1, number2));
+
+//Division
+var div = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(div(number1, number2));
